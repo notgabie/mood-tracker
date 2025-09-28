fa3e59f75e2a5a4f44d041d6cc9c095b092b7796
notgabie <gabriellamondvay@gmail.com>
2025-09-28 16:28:08 +0200

    add components for tag labels and the progress bar

---
+++ src/lib/components/ui/ProgressBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let { currentStep = $bindable(), totalSteps }: { currentStep: number; totalSteps: number } =
+		$props();
+
+	function navigateToStep(clickedStep: number) {
+		if (clickedStep <= currentStep || clickedStep === currentStep + 1) {
+			currentStep = clickedStep;
+		}
+	}
+</script>
+
+<div class="flex w-full gap-2">
+	{#each Array(totalSteps) as _, i}
+		{@const stepNumber = i + 1}
+		<button
+			class="flex h-1.5 w-1/5 rounded-md cursor-pointer relative border-transparent {currentStep >= stepNumber ? 'bg-blue-60' : 'bg-blue-20'}"
+			onclick={() => navigateToStep(stepNumber)}
+			aria-current={currentStep === stepNumber ? 'step' : undefined}
+			aria-label={`Step ${stepNumber} of ${totalSteps}`}
+		>
+			
+		</button>
+	{/each}
+</div>
---
+++ src/lib/components/ui/TagLabel.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let { id, name, value, onchange }: { id?: string; name?: string; value?: string, onchange?: () => void } = $props();
+</script>
+
+<div class="border border-blue-60 flex gap-1 items-center justify-center w-fit p-1.5 rounded-lg cursor-pointer">
+	<input type="checkbox" {id} {name} {value} class="border-blue-60 accent-bg-60 bg-blue-60 "/>
+			<label for={id} class="text-sm">{name}</label>
+</div>
